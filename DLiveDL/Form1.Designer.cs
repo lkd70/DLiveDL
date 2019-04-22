5d67b4993de3a56169c91153ae70bb4ff72c4fd4
LKD70 <lkdornan70@gmail.com>
2019-04-23 00:34:49 +0100

    Initial Release

---
+++ DLiveDL/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace DLiveDL
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.lblURL = new System.Windows.Forms.Label();
+            this.lblFile = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.lblFormat = new System.Windows.Forms.Label();
+            this.brnDownload = new System.Windows.Forms.Button();
+            this.lblProgress = new System.Windows.Forms.Label();
+            this.sfdBrowse = new System.Windows.Forms.SaveFileDialog();
+            this.btnBrowseFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtURL
+            // 
+            this.txtURL.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtURL.Location = new System.Drawing.Point(42, 6);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(223, 20);
+            this.txtURL.TabIndex = 0;
+            // 
+            // lblURL
+            // 
+            this.lblURL.AutoSize = true;
+            this.lblURL.Location = new System.Drawing.Point(4, 6);
+            this.lblURL.Name = "lblURL";
+            this.lblURL.Size = new System.Drawing.Size(32, 13);
+            this.lblURL.TabIndex = 1;
+            this.lblURL.Text = "URL:";
+            // 
+            // lblFile
+            // 
+            this.lblFile.AutoSize = true;
+            this.lblFile.Location = new System.Drawing.Point(10, 35);
+            this.lblFile.Name = "lblFile";
+            this.lblFile.Size = new System.Drawing.Size(26, 13);
+            this.lblFile.TabIndex = 3;
+            this.lblFile.Text = "File:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(191, 32);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(75, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // lblFormat
+            // 
+            this.lblFormat.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblFormat.AutoSize = true;
+            this.lblFormat.Location = new System.Drawing.Point(143, 35);
+            this.lblFormat.Name = "lblFormat";
+            this.lblFormat.Size = new System.Drawing.Size(42, 13);
+            this.lblFormat.TabIndex = 5;
+            this.lblFormat.Text = "Format:";
+            // 
+            // brnDownload
+            // 
+            this.brnDownload.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.brnDownload.Location = new System.Drawing.Point(191, 59);
+            this.brnDownload.Name = "brnDownload";
+            this.brnDownload.Size = new System.Drawing.Size(75, 23);
+            this.brnDownload.TabIndex = 6;
+            this.brnDownload.Text = "Download";
+            this.brnDownload.UseVisualStyleBackColor = true;
+            this.brnDownload.Click += new System.EventHandler(this.brnDownload_Click);
+            // 
+            // lblProgress
+            // 
+            this.lblProgress.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblProgress.AutoSize = true;
+            this.lblProgress.Location = new System.Drawing.Point(6, 88);
+            this.lblProgress.Name = "lblProgress";
+            this.lblProgress.Size = new System.Drawing.Size(0, 13);
+            this.lblProgress.TabIndex = 8;
+            // 
+            // btnBrowseFile
+            // 
+            this.btnBrowseFile.Location = new System.Drawing.Point(42, 30);
+            this.btnBrowseFile.Name = "btnBrowseFile";
+            this.btnBrowseFile.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowseFile.TabIndex = 9;
+            this.btnBrowseFile.Text = "Browse";
+            this.btnBrowseFile.UseVisualStyleBackColor = true;
+            this.btnBrowseFile.Click += new System.EventHandler(this.btnBrowseFile_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 107);
+            this.Controls.Add(this.btnBrowseFile);
+            this.Controls.Add(this.lblProgress);
+            this.Controls.Add(this.brnDownload);
+            this.Controls.Add(this.lblFormat);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.lblFile);
+            this.Controls.Add(this.lblURL);
+            this.Controls.Add(this.txtURL);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(600, 140);
+            this.MinimumSize = new System.Drawing.Size(290, 140);
+            this.Name = "frmMain";
+            this.Text = "DLive DL - LKD70";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Label lblURL;
+        private System.Windows.Forms.Label lblFile;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label lblFormat;
+        private System.Windows.Forms.Button brnDownload;
+        private System.Windows.Forms.Label lblProgress;
+        private System.Windows.Forms.SaveFileDialog sfdBrowse;
+        private System.Windows.Forms.Button btnBrowseFile;
+    }
+}
+
---
+++ DLiveDL/Form1.cs
@@ -0,0 +1,101 @@
+﻿using NReco.VideoConverter;
+using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace DLiveDL
+{
+    public partial class frmMain : Form
+    {
+        FFMpegConverter ffmpeg = new FFMpegConverter();
+        string selected_format = Format.mp4;
+        string file_name = "";
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            ffmpeg.ConvertProgress += UpdateProgress;
+            comboBox1.Items.Add("rawvideo");
+            comboBox1.Items.Add("webm");
+            comboBox1.Items.Add("mov");
+            comboBox1.Items.Add("flv");
+            comboBox1.Items.Add("mp4");
+            comboBox1.SelectedIndex = 4;
+        }
+
+        private void brnDownload_Click(object sender, EventArgs e)
+        {
+            if (!isUrlValid(txtURL.Text))
+            {
+                MessageBox.Show("URL isn't valid. Please copy and paste the URL exactly as it is shown.");
+                return;
+            }
+
+            if (sfdBrowse.FileName == "")
+            {
+                MessageBox.Show("Please click 'browse' and find a suitable directory and name for your file.");
+                return;
+            }
+            else
+            {
+                file_name = sfdBrowse.FileName;
+            }
+
+            new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+                ffmpeg.ConvertMedia(txtURL.Text, file_name, selected_format);
+            }).Start();
+        }
+
+        private void btnBrowseFile_Click(object sender, EventArgs e)
+        {
+            sfdBrowse.DefaultExt = comboBox1.SelectedItem.ToString();
+            sfdBrowse.ShowDialog();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selected_format = Format.mp4;
+            if (comboBox1.SelectedIndex == 0) selected_format = Format.raw_video;
+            if (comboBox1.SelectedIndex == 1) selected_format = Format.webm;
+            if (comboBox1.SelectedIndex == 2) selected_format = Format.mov;
+            if (comboBox1.SelectedIndex == 3) selected_format = Format.flv;
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            ffmpeg.Abort();
+        }
+
+        private void UpdateProgress(object sender, ConvertProgressEventArgs e)
+        {
+            lblProgress.BeginInvoke((MethodInvoker)delegate () {
+                lblProgress.Text = "Progress: " + e.Processed.ToString() + " / " + e.TotalDuration.ToString();
+                if (e.Processed == e.TotalDuration)
+                {
+                    lblProgress.Text = "Progress: Done";
+                    MessageBox.Show("Download completed");
+                }
+            });
+
+        }
+
+        private static double Divide(TimeSpan dividend, TimeSpan divisor)
+        {
+            return dividend.Ticks / divisor.Ticks;
+        }
+
+        private bool isUrlValid(string url)
+        {
+            Regex url_reg = new Regex(@"https:\/\/playback.prd.dlivecdn.com\/live\/[a-zA-Z0-9-_]+\/\d+\/(?:vod|master\/stitched-playlist).m3u8");
+            Match match = url_reg.Match(url);
+            return match.Success;
+        }
+    }
+}
